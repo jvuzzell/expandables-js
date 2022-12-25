977e45913cdeb291a81c9a03ede75958b659ab7a
Joshua Uzzell <josh.uzzell@gmail.com>
2022-12-25 16:13:06 -0500

    Complete tests

--- expandables.test.js
+++ expandables.test.js
@@ -1,71 +1,94 @@
-import {screen, waitFor } from '@testing-library/dom'; 
+import {screen} from '@testing-library/dom'; 
 import userEvent from '@testing-library/user-event';
 import {Expandables, initExpandables} from './expandables.js'; 
 
  function getExpandableExample() {
     return `
-        <div data-testid="expandable-1" data-expandable-container="expanded">
-            <button data-expandable-trigger="click" type="button"></button>
+        <div data-testid="expandable-1" data-expandable-container="expanded" data-expandable-override=true data-expandable-callback="exampleCallback">
+            <button data-expandable-trigger type="button" data-testid="trigger-1"></button>
             <div data-expandable-target="example-target-1"></div>
         </div> 
         <div data-testid="expandable-2" data-expandable-container="expanded">
-            <button data-expandable-trigger="click" type="button"></button>
+            <button data-expandable-trigger="" type="button" data-testid="trigger-2"></button>
             <div data-expandable-target="example-target-2"></div>
         </div>
         <div data-testid="expandable-3" data-expandable-container="expanded">
-        <button data-expandable-trigger type="button"></button>
-        <div data-expandable-target="example-target-3"></div>
-    </div>
+            <button data-expandable-trigger="mouseover" type="button" data-testid="trigger-3"></button>
+            <div data-expandable-target="example-target-3"></div>
+        </div>
         <div data-expandable-group>
             <div data-testid="expandable-4" data-expandable-container="expanded">
-                <button data-expandable-trigger="click" type="button"></button>
+                <button data-expandable-trigger="click" type="button" data-testid="group-trigger-4"></button>
                 <div data-expandable-target="example-target-4"></div>
             </div>
             <div data-testid="expandable-5" data-expandable-container="collapsed">
                 <button data-expandable-trigger="click" type="button" data-testid="group-trigger-5"></button>
                 <div data-expandable-target="example-target-5"></div>
             </div>
-            <div data-testid="expandable-6" data-expandable-container="collapsed">
-                <button data-expandable-trigger="click" type="button" data-testid="group-trigger-2"></button>
+            <div data-testid="expandable-6" data-expandable-container="expanded">
+                <button data-expandable-trigger="click" type="button" data-testid="group-trigger-6"></button>
                 <div data-expandable-target="example-target-6"></div>
             </div>
         </div>
     `;
-} 
+}
 
 beforeEach(() => { 
+    window.exampleCallback = ( event ) => {
+        let expandableId = event.target.getAttribute( 'data-expandable-id' ); 
+        Expandables.getExpandable( expandableId ).toggle();
+    }
+
     const testContainer = getExpandableExample();
     document.body.innerHTML = testContainer; 
-    initExpandables();
+    initExpandables(); 
 });
 
 afterEach(() => {
-    document.body.innerHTML = ``;
+    Expandables.destroyExpandables();
+    document.body.innerHTML = ''; 
 }); 
 
-test('Expandables initialization', async () => { 
-
-    await waitFor(() => {
-        expect(Expandables.getExpandable( 'example-target-1' ).isExpanded()).toBe(true);
-        expect(Expandables.getExpandable( 'example-target-2' ).isExpanded()).toBe(true);
-        expect(Expandables.getExpandable( 'example-target-3' ).isExpanded()).toBe(true);
-        expect(Expandables.getExpandable( 'example-target-4' ).isExpanded()).toBe(true);
-        expect(Expandables.getExpandable( 'example-target-5' ).isExpanded()).toBe(false);
-        expect(Expandables.getExpandable( 'example-target-6' ).isExpanded()).toBe(false);
-    });
-
+test('Expandable Ids attached to triggers', () => { 
+    expect(screen.getByTestId( 'trigger-1' )).toHaveAttribute('data-expandable-id', 'example-target-1');
+    expect(screen.getByTestId( 'trigger-2' )).toHaveAttribute('data-expandable-id', 'example-target-2');
+    expect(screen.getByTestId( 'trigger-3' )).toHaveAttribute('data-expandable-id', 'example-target-3');
+    expect(screen.getByTestId( 'group-trigger-4' )).toHaveAttribute('data-expandable-id', 'example-target-4');
+    expect(screen.getByTestId( 'group-trigger-5' )).toHaveAttribute('data-expandable-id', 'example-target-5');
+    expect(screen.getByTestId( 'group-trigger-6' )).toHaveAttribute('data-expandable-id', 'example-target-6');
 }); 
 
-test('Expandable group collapses when individual opened', async () => { 
+test('Expandable siblings in group collapse when target expandable opened', async () => {
+    let targetButton = screen.getByTestId( 'group-trigger-5' );   
+    let targetNode = screen.getByTestId( 'expandable-5' );  
 
-    // @Camden - This test fails. 
-    let targetButton1 = screen.getByTestId( 'group-trigger-5' );
-    let targetNode = screen.findByTestId( 'expandable-5' ); 
-    userEvent.click(targetButton1);
-    let expanded = (await targetNode).getAttribute( '[data-expandable-container]');
+    expect( targetNode.getAttribute( 'data-expandable-container' ) ).toBe('collapsed'); 
 
-    await waitFor(() => {
-        expect(expanded).toBe(true);
+    await userEvent.click(targetButton).then(() => { 
+        let targetNode = screen.getByTestId( 'expandable-5' );  
+        let siblingNode4 = screen.getByTestId( 'expandable-4' );
+        let siblingNode6 = screen.getByTestId( 'expandable-6' );
+
+        expect( targetNode.getAttribute( 'data-expandable-container' ) ).toBe('expanded'); 
+        expect( siblingNode4.getAttribute( 'data-expandable-container' ) ).toBe('collapsed');
+        expect( siblingNode6.getAttribute( 'data-expandable-container' ) ).toBe('collapsed'); 
     });
+}); 
 
+test('Expandable trigger fires custom callback', async () => {
+    let targetButton = screen.getByTestId( 'trigger-1' );   
+
+    await userEvent.click(targetButton).then(() => { 
+        let targetNode = screen.getByTestId( 'expandable-1' );    
+        expect( targetNode.getAttribute( 'data-expandable-container' ) ).toBe('collapsed'); 
+    });
+});
+
+test('Expandable triggered on mouse over', async () => {
+    let targetButton = screen.getByTestId( 'trigger-3' );   
+
+    await userEvent.hover(targetButton).then(() => { 
+        let targetNode = screen.getByTestId( 'expandable-3' );     
+        expect( targetNode.getAttribute( 'data-expandable-container' ) ).toBe('collapsed'); 
+    });
 });
